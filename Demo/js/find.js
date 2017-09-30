de0da1b49d213d693af7b75f5e9d2e3339fa3044
yuzexia <327932801@qq.com>
2017-09-30 11:04:36 +0800

    demo

---
+++ Demo/js/find.js
@@ -0,0 +1,45 @@
+$(function(){
+	var mask = $('.mask'),
+		contentMask = $('.mask-content'),
+		closeMask = $('.mask-close img');
+
+	mask.on('click',function(){
+		hideMask(mask);
+	})
+	closeMask.on('click',function(e){
+		e.stopPropagation();
+		hideMask(mask);
+	})
+	contentMask.on('click',function(e){
+		e.stopPropagation();
+	})
+
+	/*顶部nav*/
+	var swiper = new Swiper('.nav-container', {
+		slidesPerView: 'auto',
+		paginationClickable: true
+	});
+	$(".nav-ul .swiper-slide").click(function() {
+		$(this).addClass("active-li").siblings().removeClass("active-li");
+	});
+
+	var $grid
+	$(".am-gallery-con img").lazyload({
+		placeholder:null,
+		threshold:500,
+		event: 'scroll',
+		failurelimit : 10,
+		no_fake_img_loader:false,
+		load:function(){
+			$grid = $('.grid').masonry();
+			$(this).removeClass('loading');
+			$grid.imagesLoaded().progress( function() {
+				$grid.masonry('layout');
+			});
+		}
+	});
+	/* 隐藏遮罩层 */
+	function hideMask(ele){
+		ele.hide();
+	}
+});
